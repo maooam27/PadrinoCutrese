d48b9d610feb0f40a0bab64190b5815ff9ed715d
maooam27 <mfede478@gmail.com>
2023-05-02 21:06:14 +0200

    init

---
+++ main.py
@@ -0,0 +1,47 @@
+import telebot
+from platform import platform, system, release
+import subprocess
+
+BOT_TOKEN = "5729050636:AAGmzPQy2Fmpi0l-AHmxNJ2FU221omQem3M"
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Bot in esecuzione")
+
+
+@bot.message_handler(commands=['info'])
+def send_info(message):
+    bot.reply_to(message, f"Sistema: {platform()}")
+
+
+@bot.message_handler(commands=['sistema'])
+def send_sistema(message):
+    bot.reply_to(message, f"Sistema: {system()} {release()}")
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, "Comandi disponibili: \n /help : mostra l'elenco dei comandi disponibili"
+                          "\n /start : avvia il bot"
+                          "\n /info : mostra le informazioni specifiche sul sistema generale"
+                          "\n /sistema : mostra le informazioni sul sistema in uso"
+                          "\n /spegni : spegne il sistema"
+                          "\n /minecraft : chiude minecraft")
+
+
+@bot.message_handler(commands=['spegni'])
+def send_spegni(message):
+    bot.reply_to(message, f"Sistema in spegnimento capo")
+    subprocess.call(["shutdown /s /f /t 0"])
+
+
+@bot.message_handler(commands=['minecraft'])
+def send_ciaominecraft(message):
+    bot.reply_to(message, f"Minecraft in chiusura capo")
+    subprocess.call(["taskkill /IM minecraft.exe /F"])
+
+
+bot.infinity_polling()
