80a5f632fe9132b54b1cdbe94b4a0fbad59bc1c7
maooam27 <52970070+maooam27@users.noreply.github.com>
2023-05-17 16:49:43 +0200

    update system

--- launcher.py
+++ launcher.py
@@ -1,6 +1,6 @@
 import requests
 
-a = requests.get('https://raw.githubusercontent.com/maooam27/PadrinoCutrese/master/app/main.py?token=GHSAT0AAAAAACCYTSTF7STQ5VCLDTGAZ3P4ZDE6VAA')
+a = requests.get('https://github.com/maooam27/PadrinoCutrese/raw/master/app/main.py')
 
 with open("app/main.py", 'w') as f:
     f.writelines(a.text)
